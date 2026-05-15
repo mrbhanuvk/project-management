fcb0072b434188de7905f722b0912c548c50fc2f
mrbhanuvk <bhanukamble1249@gmail.com>
2026-05-15 23:34:30 +0530

    Add client folder

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { store } from './app/store.js'
+import { Provider } from 'react-redux'
+import { ClerkProvider } from '@clerk/react'
+
+console.log(import.meta.env.VITE_CLERK_PUBLISHABLE_KEY)
+
+createRoot(document.getElementById('root')).render(
+    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
+    </ClerkProvider>
+)
