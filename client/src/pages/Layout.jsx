0e64b880d7de906e5285df7e9ab7ee332998dcda
mrbhanuvk <bhanukamble1249@gmail.com>
2026-05-17 13:35:24 +0530

    fix: move routes to src/, fix server.js imports, add client config

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -5,23 +5,26 @@ import { Outlet } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { loadTheme } from '../features/themeSlice'
 import { Loader2Icon } from 'lucide-react'
-import { useUser, SignIn } from '@clerk/react'
+import { useUser, SignIn, useAuth, CreateOrganization } from '@clerk/react'
+import { fetchWorkspaces } from '../features/workspaceSlice'
 
 const Layout = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-
-    const { loading } = useSelector((state) => state.workspace)
-
+    const { loading, workspaces } = useSelector((state) => state.workspace)
     const dispatch = useDispatch()
-
     const { user, isLoaded } = useUser()
+    const { getToken } = useAuth()
 
-    // Initial load of theme
     useEffect(() => {
         dispatch(loadTheme())
     }, [dispatch])
 
-    // Clerk loading
+    useEffect(() => {
+        if (isLoaded && user && workspaces.length === 0) {
+            dispatch(fetchWorkspaces({ getToken }))
+        }
+    }, [user, isLoaded])
+
     if (!isLoaded) {
         return (
             <div className='flex items-center justify-center h-screen bg-white dark:bg-zinc-950'>
@@ -30,7 +33,6 @@ const Layout = () => {
         )
     }
 
-    // Not signed in
     if (!user) {
         return (
             <div className='flex items-center justify-center h-screen bg-white dark:bg-zinc-950'>
@@ -39,11 +41,18 @@ const Layout = () => {
         )
     }
 
-    // App loading
     if (loading) {
         return (
             <div className='flex items-center justify-center h-screen bg-white dark:bg-zinc-950'>
                 <Loader2Icon className="size-7 text-blue-500 animate-spin" />
+            </div>
+        )
+    }
+
+    if (user && workspaces.length === 0) {
+        return (
+            <div className='min-h-screen flex justify-center items-center'>
+                <CreateOrganization />
             </div>
         )
     }
@@ -54,13 +63,11 @@ const Layout = () => {
                 isSidebarOpen={isSidebarOpen}
                 setIsSidebarOpen={setIsSidebarOpen}
             />
-
             <div className="flex-1 flex flex-col h-screen">
                 <Navbar
                     isSidebarOpen={isSidebarOpen}
                     setIsSidebarOpen={setIsSidebarOpen}
                 />
-
                 <div className="flex-1 h-full p-6 xl:p-10 xl:px-16 overflow-y-scroll">
                     <Outlet />
                 </div>
