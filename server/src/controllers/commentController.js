6536c095fa6e36600bf012ab8726b3d1e0f31d47
mrbhanuvk <bhanukamble1249@gmail.com>
2026-05-18 13:18:37 +0530

    add all controllers, routes, middleware and client updates

---
+++ server/src/controllers/commentController.js
@@ -0,0 +1,40 @@
+//add comment
+
+import prisma from "../config/prisma";
+
+export const addComment=async(req,res)=>{
+    try {
+        const {userId}=await req.auth();
+        const {content,taskId}=req.body
+
+        //check if user is project member
+        const task =await prisma.task.findUnique({
+            where:{id:taskId},
+        })
+
+        const project=await prisma.project.findUnique({
+            where:{id:task.projectId},
+            include: {members: {include: {user: true}}}
+        })
+
+        if(!project){
+            return res.status(404).json({message: "Project not found"});
+        }
+
+        const member=project.member.find((member)=>member.userId===userId);
+        if(!member){
+            return res.status(403).json({message: "You are not member of this project"});
+        }
+
+        const comment=await prisma.comment.create({data: {taskId,comment,userId},
+            data:{taskId,content,userId},
+            include: {user:true}
+
+        })
+
+        res.json({comment})
+    } catch (error) {
+        console.log(error);
+    res.status(500).json({ message: error.code || error.message });
+    }
+}
