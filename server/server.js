1ecb4dc7af1dcf3795c2fe10fe9afd1c433ae57c
mrbhanuvk <bhanukamble1249@gmail.com>
2026-05-19 00:31:48 +0530

    fix: add staffsync-lake.vercel.app to CORS origins

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ import commentRouter from './src/routes/commentRoutes.js';
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://project-mgt-olive.vercel.app'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://project-mgt-olive.vercel.app', 'https://staffsync-lake.vercel.app'],
   credentials: true,
 }));
 app.use(express.json());
